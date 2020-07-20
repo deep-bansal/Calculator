8e324c09d97d9c04de90c7013ffd6631f0d27ecf
Deep Bansal <66458119+deep-bansal@users.noreply.github.com>
2020-07-20 11:48:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,3 +94,82 @@ for(var i=0; i<buttons.length; i++)
     });
 }
 
+ document.addEventListener('keypress',function(event)
+    {
+        var value = event.keyCode;
+
+         if(value == 43)
+        {            
+            operator = '+';
+            operand1 = parseFloat(display.innerText);
+            display.innerText="";
+        }
+
+        else if(value == 37)
+        {
+            var val = parseFloat(display.innerText);
+            val = val/100;
+            display.innerText = "";
+            display.innerText = val;           
+        }
+
+        
+
+        else if(value == 47)
+        {
+            operator = '/';
+            operand1 = parseFloat(display.innerText);
+            display.innerText="";           
+        }
+
+
+        else if(value == 42)
+        {
+            operator = '*';
+            operand1 = parseFloat(display.innerText);
+            display.innerText="";           
+        }
+        
+        else if(value == 45)
+        {
+            operator = '-';
+            operand1 = parseFloat(display.innerText);
+            display.innerText="";           
+        }
+
+        else if(value == 13)
+        {
+            operand2 = parseFloat(display.innerText);
+            if(operator =='/' && operand2 == '0')
+           {
+               result = "error";
+           }
+           else
+           {
+                result = eval(operand1 + " " + operator + " " + operand2);
+
+           }
+           display.innerText="";
+           display.innerText= result;
+           operand1 = result;
+           operator = null;
+           operand2 = null;
+
+
+        }
+
+        else if (value >=48 && value<=57) {
+               
+            if(display.innerText == '0')
+            {
+                display.innerText = "";
+            }
+
+            display.innerText += value-48;
+        }
+        else{
+            display.innerText = "error"
+        }
+
+    });
+
